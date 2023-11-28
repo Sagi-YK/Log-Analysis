84cb0466ffacd1cf545eb5570555acbdca412f91
sagi yaakov <sagiyaakov2@gmail.com>
2023-11-28 18:19:11 +0200

    adding paintuml module that create sequence diagram

--- load_csv.py
+++ load_csv.py
@@ -1,8 +1,9 @@
 import csv
 import re
+from plantuml import PlantUML
 
 
-def extract_data_from_csv(csv_file_path):
+def extract_process_names_from_log(csv_file_path):
     # Set to store unique data extracted from column 4 (assuming 0-based indexing)
     unique_data_set = set()
 
@@ -65,14 +66,41 @@ def extract_data_from_csv(csv_file_path):
 #     for output_file in output_files.values():
 #         output_file.close()
 
+def create_sequence_diagram():
+    plantuml = PlantUML(url='http://www.plantuml.com/plantuml/img/')
+    plantuml.processes_file(filename='sample.txt', outfile='sequence_diagram.png')
+
 
 def main():
     csv_file_path = "Logs\\LNL-M_WarmReset_log.csv"
 
-    result = extract_data_from_csv(csv_file_path)
-    print(result)
-    
-    # sort_csv_file(file_path)
+    process_names = extract_process_names_from_log(csv_file_path)
+    # print(process_names)
+
+    example = 'Python->CSME: Start\n'
+    example += 'activate CSME\n'
+    example += 'CSME->OSSE: Authentication Request\n'
+    example += 'activate OSSE\n'
+    example += 'OSSE->ESE: Authentication Request\n'
+    example += 'activate ESE\n'
+    example += 'ESE->OSSE: Authentication Response\n'
+    example += 'deactivate ESE\n'
+    # example += 'note right of OSSE: OSSE thinks about it\n'
+    example += 'OSSE->CSME: Authentication Response\n'
+    example += 'deactivate OSSE\n'
+    example += 'CSME->BIOS: run\n'
+    example += 'activate BIOS\n'
+    example += 'BIOS->CSME\n'
+    example += 'deactivate BIOS\n'
+    example += 'CSME->Python: End\n'
+    example += 'deactivate CSME\n'
+    try:
+        with open('sample.txt', 'w') as sequence_diagram_file:
+            sequence_diagram_file.write(example)
+    except FileNotFoundError:
+        print("The 'docs' directory does not exist")
+
+    create_sequence_diagram()
 
 
 if __name__ == "__main__":
